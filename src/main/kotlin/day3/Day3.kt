80699b20b6b099854df3640cdaa26b8945accae1
Kosta Fortumanov <kfortumanov@gmail.com>
2022-12-03 11:15:12 +0100

    Day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,37 @@
+package day3
+
+import readFileLines
+
+fun main() {
+    val input = readFileLines("input_3.txt")
+
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>) = input.sumOf {
+    val mid = it.length / 2
+    val firstHalf = it.substring(0 until mid)
+    val secondHalf = it.substring(mid)
+    val commonItem = firstHalf.first { char -> char in secondHalf }
+
+    itemPriority(commonItem)
+}
+
+private fun part2(input: List<String>) = input.chunked(3).sumOf {
+    val (elf1, elf2, elf3) = it
+    val commonItem = elf1.first { char -> char in elf2.filter { char1 -> char1 in elf3 } }
+
+    itemPriority(commonItem)
+}
+
+private fun itemPriority(item: Char): Int {
+    val lowerCaseStartPriority = 1
+    val upperCaseStartPriority = 27
+
+    return if (item.isLowerCase()) {
+        item.code - 'a'.code + lowerCaseStartPriority
+    } else {
+        item.code - 'A'.code + upperCaseStartPriority
+    }
+}
